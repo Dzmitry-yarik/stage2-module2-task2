db927c32822cd990aafd026f0fcb98ec52225bac
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-05-04 18:35:39 +0300

    Update AuthFilter.java

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -3,6 +3,7 @@ package com.example.filter;
 import com.example.Users;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
