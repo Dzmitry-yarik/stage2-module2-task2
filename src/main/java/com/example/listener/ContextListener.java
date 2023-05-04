1932c134fc79e2f3bb9f11cc10bc099e6012d13b
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-05-04 18:30:44 +0300

    Update ContextListener.java

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,18 +1,14 @@
 package com.example.listener;
 
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
 import javax.servlet.annotation.WebListener;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
 import java.time.LocalDateTime;
 
 @WebListener
-public class ContextListener implements HttpSessionListener {
-    @Override
-    public void sessionCreated(HttpSessionEvent se) {
-        HttpSession session = se.getSession();
+public class ContextListener implements ServletContextAttributeListener {
+    public void attributeAdded(ServletContextAttributeEvent event) {
 
-        session.setAttribute("servletTimeInit", LocalDateTime.now());
-
+        event.getServletContext().setAttribute("servletTimeInit", LocalDateTime.now());
     }
 }
